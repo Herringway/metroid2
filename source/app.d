4b387e934a83f1bcfed10aa721312c48d7fe7e10
Cameron Ross <elpenguino@gmail.com>
2023-11-20 14:43:39 -0400

    [debugging] add some enemy info

--- source/app.d
+++ source/app.d
@@ -8,6 +8,8 @@ import librehome.gameboy;
 import d_imgui.imgui_h;
 import ImGui = d_imgui;
 
+import std.format;
+
 struct GameSettings {}
 
 void main() {
@@ -41,6 +43,44 @@ void debugFunction(const UIState state) {
 			bool flagEnabled = !!(samusItems & mask);
 			if (ImGui.Checkbox(flagName, &flagEnabled)) {
 				samusItems = cast(ubyte)((samusItems & ~mask) | (flagEnabled * mask));
+			}
+		}
+		ImGui.TreePop();
+	}
+	if (ImGui.TreeNode("Enemies")) {
+		foreach (idx, enemy; enemyDataSlots) {
+			if ((enemy.status & 0xF) == 0) {
+				if (ImGui.TreeNode(format!"Enemy %s"(idx))) {
+					InputEditable("Status", enemy.status);
+					InputEditable("Coords", enemy.x, enemy.y);
+					InputEditable("Type", enemy.spriteType);
+					InputEditable("Attributes", enemy.baseSpriteAttributes);
+					InputEditable("Attributes (2)", enemy.spriteAttributes);
+					InputEditable("Stun Counter", enemy.stunCounter);
+					InputEditable("Misc var", enemy.misc);
+					InputEditable("Direction flags", enemy.directionFlags);
+					InputEditable("Counter", enemy.counter);
+					InputEditable("State", enemy.state);
+					InputEditable("Ice Counter", enemy.iceCounter);
+					InputEditable("Health", enemy.health);
+					InputEditable("Drop type", enemy.dropType);
+					InputEditable("Explosion flag", enemy.explosionFlag);
+					InputEditable("Screen", enemy.xScreen, enemy.yScreen);
+					InputEditable("Max health", enemy.maxHealth);
+					InputEditable("u12", enemy.u12);
+					InputEditable("u13", enemy.u13);
+					InputEditable("u14", enemy.u14);
+					InputEditable("u15", enemy.u15);
+					InputEditable("u16", enemy.u16);
+					InputEditable("u17", enemy.u17);
+					InputEditable("u18", enemy.u18);
+					InputEditable("u19", enemy.u19);
+					InputEditable("u1A", enemy.u1A);
+					InputEditable("u1B", enemy.u1B);
+					InputEditable("Spawn flag", enemy.spawnFlag);
+					InputEditable("Spawn number", enemy.spawnNumber);
+					ImGui.TreePop();
+				}
 			}
 		}
 		ImGui.TreePop();
