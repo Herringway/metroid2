1553f726e9f8f4351d8284953c57ad30727d6218
Cameron Ross <elpenguino@gmail.com>
2023-11-27 00:07:26 -0400

    add more debugging code

--- source/app.d
+++ source/app.d
@@ -1,4 +1,5 @@
 import metroid2.bank00;
+import metroid2.bank03;
 import metroid2.data;
 import metroid2.external;
 
@@ -7,6 +8,7 @@ import librehome.gameboy;
 
 import d_imgui.imgui_h;
 import ImGui = d_imgui;
+import imgui.hexeditor;
 
 import std.format;
 
@@ -26,9 +28,11 @@ void main() {
 void debugFunction(const UIState state) {
 	import metroid2.defs;
 	import metroid2.globals;
-	ImGui.SetNextWindowSize(ImGui.ImVec2(state.width, state.height));
-	ImGui.SetNextWindowPos(ImGui.ImVec2(state.x, state.y));
-	ImGui.Begin("Debugging", null, ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoBringToFrontOnFocus);
+
+	ImGui.SetNextWindowSize(ImGui.ImVec2(400 * state.scaleFactor, state.height));
+	ImGui.SetNextWindowPos(ImGui.ImVec2(0, 0));
+	ImGui.Begin("Debugging", null, ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoBringToFrontOnFocus | ImGuiWindowFlags.NoSavedSettings);
+	ImGui.InvisibleButton("padding", ImGui.ImVec2(1, ImGui.GetCursorPos().y));
 	if (ImGui.TreeNode("Samus")) {
 		InputEditable("Pose", samusPose);
 		InputEditable("Health", samusCurHealth);
@@ -50,8 +54,44 @@ void debugFunction(const UIState state) {
 		ImGui.TreePop();
 	}
 	if (ImGui.TreeNode("Enemies")) {
+		InputEditable("Enemy count", numEnemies.total);
+		InputEditable("Active enemy count", numEnemies.active);
+		InputEditable("Inactive enemy count", numEnemies.offscreen);
+		if (ImGui.TreeNode("Spawn candidates")) {
+			static void addEnemySpawns(const(EnemySpawn)[] spawns) {
+				foreach (idx, enemy; spawns) {
+					if (enemy.spawnNumber == 0xFF) {
+						break;
+					}
+					ImGui.SeparatorText(format!"Spawn %s"(idx));
+					InputEditableReadOnly("Spawn Number", enemy.spawnNumber);
+					InputEditableReadOnly("Actor", enemy.id);
+					InputEditableReadOnly("Coords", enemy.x, enemy.y);
+				}
+			}
+			ImGui.SeparatorText("Top-left");
+			addEnemySpawns(loadEnemyGetBankOffset()[cast(ubyte)((topEdge.screen << 4) | leftEdge.screen)]);
+			ImGui.SeparatorText("Top-right");
+			addEnemySpawns(loadEnemyGetBankOffset()[cast(ubyte)((topEdge.screen << 4) | ((leftEdge.screen + 1) & 0xF))]);
+			ImGui.SeparatorText("Bottom-left");
+			addEnemySpawns(loadEnemyGetBankOffset()[cast(ubyte)((((topEdge.screen + 1) & 0xF) << 4) | leftEdge.screen)]);
+			ImGui.SeparatorText("Bottom-right");
+			addEnemySpawns(loadEnemyGetBankOffset()[cast(ubyte)((((topEdge.screen + 1) & 0xF) << 4) | ((leftEdge.screen + 1) & 0xF))]);
+			ImGui.TreePop();
+		}
+		if (ImGui.TreeNode("Spawn flags")) {
+			ImGui.SeparatorText("Ephemeral");
+			foreach (idx, chunk; cast(ubyte[4][])enemySpawnFlagsUnsaved[]) {
+				InputEditable(format!"%02X"(idx * 4), chunk[0], chunk[1], chunk[2], chunk[3]);
+			}
+			ImGui.SeparatorText("Permanent");
+			foreach (idx, chunk; cast(ubyte[4][])enemySpawnFlagsSaved[]) {
+				InputEditable(format!"%02X"(idx * 4), chunk[0], chunk[1], chunk[2], chunk[3]);
+			}
+			ImGui.TreePop();
+		}
 		foreach (idx, enemy; enemyDataSlots) {
-			if ((enemy.status & 0xF) == 0) {
+			if ((enemy.status & 0xF) != 0xF) {
 				if (ImGui.TreeNode(format!"Enemy %s"(idx))) {
 					InputEditable("Status", enemy.status);
 					InputEditable("Coords", enemy.x, enemy.y);
@@ -84,6 +124,11 @@ void debugFunction(const UIState state) {
 					ImGui.TreePop();
 				}
 			}
+		}
+		if (ImGui.TreeNode("Metroid state")) {
+			InputEditable("Metroid fight active", metroidFightActive);
+			InputEditable("Metroid state", metroidState);
+			ImGui.TreePop();
 		}
 		ImGui.TreePop();
 	}
